c3a75be9115dca0dbb56917d3992d583b8ea36c2
Dmitriy25 <ohapkin_da@mail.ru>
2020-02-06 14:04:13 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hw31calculatorinterface/User.java
@@ -0,0 +1,27 @@
+package com.example.hw31calculatorinterface;
+
+import java.util.Objects;
+
+public class User {
+    String name;
+    int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age &&
+                Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
